01e2d2a6ae482fcfcb9bc621d24408d7e0cbf162
marianne <mariannehaguiar@gmail.com>
2023-04-04 22:53:04 -0300

    parse datetime to data object

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  require 'date'
+
+  before_save :date_added_as_datetime
+
+  private
+
+  def date_added_as_datetime
+    DateTime.parse(date_added.to_s)
+  end
 end
