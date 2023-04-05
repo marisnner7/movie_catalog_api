d04c14d9a96a55f11f3a70972325f880b61dd2a5
marianne <mariannehaguiar@gmail.com>
2023-04-04 22:52:24 -0300

    added functions to controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,22 @@
+class MoviesController < ApplicationController
+  require 'csv'
+
+  def create_from_csv
+    csv_file = params[:file]
+    csv_text = csv_file.read
+    csv = CSV.parse(csv_text, headers: true)
+    csv.each do |row|
+      Movie.create!(category: row['category'], title: row['title'], director: row['director'],
+        cast: row['cast'], country: row['country'], date_added: row['date_added'],
+        release_year: row['release_year'], rating: row['rating'], duration: row['duration'], 
+        listed_in: row['listed_in'], description: row['description'], 
+        year: row['year'])
+    end
+    head :ok
+  end
+
+  def index
+    movies = Movie.all
+    render json: movies
+  end
+end
