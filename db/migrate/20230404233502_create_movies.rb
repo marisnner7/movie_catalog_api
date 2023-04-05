d51f2d247e7fe1256a544f28f9238a80f6f9e818
marianne <mariannehaguiar@gmail.com>
2023-04-04 22:52:47 -0300

    fix schema to reflect csv file

--- db/migrate/20230404233502_create_movies.rb
+++ db/migrate/20230404233502_create_movies.rb
@@ -1,13 +1,19 @@
 class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
+      t.string :category
       t.string :title
-      t.text :description
-      t.integer :year
-      t.string :genre
+      t.string :director
+      t.text :cast
       t.string :country
       t.datetime :date_added
-
+      t.string :release_year
+      t.string :rating
+      t.string :duration
+      t.string :listed_in
+      t.text :description
+      t.integer :year
+  
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_233502) do
   enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
+    t.string "category"
     t.string "title"
-    t.text "description"
-    t.integer "year"
-    t.string "genre"
+    t.string "director"
+    t.text "cast"
     t.string "country"
     t.datetime "date_added"
+    t.string "release_year"
+    t.string "rating"
+    t.string "duration"
+    t.string "listed_in"
+    t.text "description"
+    t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
